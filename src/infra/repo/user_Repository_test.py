dcc17e195a847d20b08fcf56d8093182bd9dfc7f
Carlos Eduardo - Kadu <carlos.spadilha@yahoo.com.br>
2023-04-25 21:59:54 -0300

    acertando pytest

--- src/infra/repo/user_Repository_test.py
+++ src/infra/repo/user_Repository_test.py
@@ -15,24 +15,34 @@ def test_insert_user():
 
     name = faker.name()
     password = faker.word()
-    engine = db_connection_handler.get_engine()
+    # engine = db_connection_handler.get_engine()
 
     # SQL comands
     new_user = user_repostory.insert_user(name, password)
 
+    assert new_user.name == name
+    assert new_user.password == password
+
+
+def test_select_user():
+    """Select in users"""
+
+    engine = db_connection_handler.get_engine()
+
     with engine.connect() as connection:
         # select data in users
-        query_user = connection.execute(
-            text(f"SELECT * FROM users WHERE id={new_user.id};")
-        ).fetchone()
+        query_user = connection.execute(text(f"SELECT * FROM users;"))
 
-        # deleting data of select in users
-        connection.execute(text(f"DELETE FROM users WHERE id={new_user.id} ;"))
+    for us in query_user:
+        print(us.name, us.id)
+
+
+def test_delete_user():
+    """deleting data in users"""
+
+    engine = db_connection_handler.get_engine()
+
+    """ deleting data of select in users """
+    with engine.connect() as connection:
+        connection.execute(text(f"DELETE FROM users WHERE id>{0} ;"))
         connection.commit()
-
-    print(new_user)
-    print(query_user)
-
-    assert new_user.id == query_user.id
-    assert new_user.name == query_user.name
-    assert new_user.password == query_user.password
