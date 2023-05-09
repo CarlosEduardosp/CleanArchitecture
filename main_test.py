cef39dc789e25dd648bb9754c7ff0977567b1cfe
Carlos Eduardo - Kadu <carlos.spadilha@yahoo.com.br>
2023-05-08 23:37:25 -0300

    atualizando

---
+++ main_test.py
@@ -0,0 +1,9 @@
+from src.infra.config import DBConnectionHandler, Base
+
+
+def test_criar_banco():
+
+    # criando o banco de dados.
+    db_conn = DBConnectionHandler()
+    engine = db_conn.get_engine()
+    Base.metadata.create_all(engine)
